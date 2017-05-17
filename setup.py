1a1d1edb4da145c1082ba7d642905414ed0e29e0
Stephen Weber <stephen.weber@jivesoftware.com>
2017-05-16 22:34:12 -0700

    Rework project layout into package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='gophersnake',
+    version='1.0.0',
+    description='A gopher server written in Python',
+    license='MIT',
+    packages=['gophersnake'],
+)
